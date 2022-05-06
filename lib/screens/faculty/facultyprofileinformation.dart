dec844870cde4a43f7dbb44513d726ce6bae72c0
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2022-05-06 15:34:47 +0530

    Some Minor Changes

--- lib/screens/faculty/facultyprofileinformation.dart
+++ lib/screens/faculty/facultyprofileinformation.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reliable_app/models/admin_login_model.dart';
+
+import '../../controllers/providers/user_provider.dart';
 
 class FacultyProfileInformation extends StatefulWidget {
   static const String routeName = "/FacultyProfileInformation";
@@ -12,12 +16,14 @@ class FacultyProfileInformation extends StatefulWidget {
 class _FacultyProfileInformationState extends State<FacultyProfileInformation> {
   @override
   Widget build(BuildContext context) {
+    AdminLoginModel adminLoginModel = Provider.of<UserProvider>(context, listen: true).adminLoginModel ?? AdminLoginModel();
+
     return Scaffold(
       body: SafeArea(
           child: Container(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Text('Faculty Profile Information Page'),
+              child: Text('Faculty Profile Information Page:${adminLoginModel.name}'),
             ),
           )
       ),
--- lib/screens/students/studentprofileinformation.dart
+++ lib/screens/students/studentprofileinformation.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reliable_app/controllers/providers/user_provider.dart';
+import 'package:reliable_app/models/student_model.dart';
 
 class StudentProfileInformation extends StatefulWidget {
   static const String routeName = "/StudentProfileInformation";
@@ -12,12 +15,14 @@ class StudentProfileInformation extends StatefulWidget {
 class _StudentProfileInformationState extends State<StudentProfileInformation> {
   @override
   Widget build(BuildContext context) {
+    StudentModel studentModel = Provider.of<UserProvider>(context, listen: true).studentModel ?? StudentModel();
+
     return Scaffold(
       body: SafeArea(
           child: Container(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Text('Student Profile Information Page'),
+              child: Text('Student Profile Information Page:${studentModel.name}'),
             ),
           )
       ),
